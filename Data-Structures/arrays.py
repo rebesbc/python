2d198d2e20a0a4a79522e68d1dff77b54502aa06
rebesbc <19170549@itculiacan.edu.mx>
2023-09-09 21:37:01 -0700

    feat: Data Structures, Arrays

---
+++ Data-Structures/arrays.py
@@ -0,0 +1,12 @@
+new_list = [1, 2, 3]
+result = new_list [0]
+
+# maneras de buscar un elemento en el arreglo
+# búsquedas lineales
+if 1 in new_list: print(True)
+
+for n in new_list:
+    if n == 1:
+        print(True)
+
+        break
