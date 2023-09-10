94d9bb7b62d4382513feac5e6c75c8358296e965
rebesbc <19170549@itculiacan.edu.mx>
2023-09-09 23:09:32 -0700

    feat: Prepend Linked Lists

--- Data-Structures/linked_list.py
+++ Data-Structures/linked_list.py
@@ -22,12 +22,29 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __repr__(self):
+        # devuelve una representación de la lista en String
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        # agrupa todas las cadenas de nodos en una sola cadena String
+        return '-> '.join(nodes)
+
     def is_empty(self):
         return self.head == None
     
     def size(self):
         # devuelve el número de nodos que hay en la lista
-        
+
         current = self.head
         count = 0
 
@@ -37,3 +54,9 @@ class LinkedList:
             current = current.next_node
         
         return count
+    
+    def add (self, data):
+        # añade un nuevo nodo al comienzo de la lista
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
