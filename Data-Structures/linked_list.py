36bf80e9532c97ed1840cf2b1ab8505c962b0a94
rebesbc <19170549@itculiacan.edu.mx>
2023-09-11 11:39:28 -0700

    feat: Insert, Remove Nodes from Linked List

--- Data-Structures/linked_list.py
+++ Data-Structures/linked_list.py
@@ -60,3 +60,55 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def search(self, key):
+        # busca el primer nodo que contenga el dato que coincida con el parámetro 'key'
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+    
+    def insert(self, data, index):
+        # inserta un nuevo nodo en un índice cualquiera deseado
+        if index == 0:
+            self.add(data)
+        
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = node.next_node
+                position -= 1
+            
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        # elimina el nodo que contenga la información deseada
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
+
