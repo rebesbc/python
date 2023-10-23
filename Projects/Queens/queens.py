f8952bd6221ebe54fa7d8eab086c7596daf3a7ea
rebesbc <19170549@itculiacan.edu.mx>
2023-10-23 09:56:24 -0700

    feat: Greedy Queens 50x50

--- Projects/Queens/queens.py
+++ Projects/Queens/queens.py
@@ -1,61 +1,27 @@
-# python terminal test:
-# > python -i Projects/Queens/queens.py
-# > dls(f, max_tries)
-# > exit()
+n = 50                  # tablero (n * n)
+f = [0] * n             # [0, 0, 0, 0], n = 4
+blacklist = []          # configuraciones visitadas
 
-from tree import Node
-
-n = 5                   # tablero (n * n)
-max_tries = 30          # límite de iteraciones a probar
-root = Node([1] * n)    # [1, 1, 1, 1], n = 4
-root.level = 0
-f = [root.data]         # frontier en estado inicial
-visited = []            # nodos visitados
-
-
-# Depth Limited Search
-def dls(frontier, limit):
-    if len(frontier) == 0:
+# Greedy Search
+def greedys(frontier):
+    print("\n- state: ", frontier)
+    if not frontier:
         print("No hay solución")
         return False
     else:
-        actual_state = frontier.pop(0)
-        node = root.get_node(actual_state)
-        actual_states_level = node.level
-        visited.append(actual_state)
+        actual_state = frontier
         if goal_test(actual_state):
             print("Se encontró la solución para ", n, " reinas: ")
             print(actual_state)
             return True
         else:
-            if limit > actual_states_level:
-                print("- actual_state: ", actual_state)
-                offsprings = expand(actual_state)
-                print("- offsprings: ", offsprings)
-                assign_level(offsprings, actual_states_level + 1)
-                if len(offsprings) > 0:
-                    if frontier:
-                        frontier[:0] = offsprings
-                    else:
-                        frontier = offsprings[:]
-                print("- frontier: ", frontier, "\n")
-                dls(frontier, limit)
-            else:
-                print("No se halló solución para ", n, " reinas en ", limit, " iteraciones")
-                return False
-            
-
-# Iterative Deepening Depth First Search
-def iddfs():
-    limit = 2
-    visited.clear()
-    ok = False
-
-    while not ok:
-        frontier = [root.data]
-        print("## Probando con un límite de ", limit, " ##")
-        ok = dls(frontier, limit)
-        limit += 2
+            offsprings = expand(actual_state)
+            order = evaluate(offsprings)
+            offsprings = bubble_sort(offsprings, order)
+        if offsprings:
+            greedys(offsprings[0])
+        else:
+            greedys([])
 
 
 def goal_test(test):
@@ -63,37 +29,53 @@ def goal_test(test):
 
 
 def expand(configuration):
-    offsprings = []
+    copy = configuration.copy()
+    offspring_states = []
+    blacklist.append(configuration)
 
-    for i in range(n):
-        child = list(configuration)
-        element = child[i]
-        if (element <= n - 1):
-            element += 1
-            child[i] = element
-            if child not in visited:
-                offsprings.append(child)
+    for row in range(0, n):
+        for column in range(0, n):
+            # Mueve la reina en la columna actual
+            new_value = (configuration[column] + row) % n
+            configuration[column] = new_value
 
-                # agregar al árbol cada hijo-offspring
-                father = root.get_node(configuration)
-                father.add_child(Node(child))
+            if configuration not in blacklist:
+                offspring_states.append(configuration)
 
+            configuration = copy.copy()
+
+    return offspring_states
+
+
+def evaluate(offsprings):
+    number_attacks = []
+    for child in offsprings:
+        number_attacks.append(attacks(child))
+    
+    return number_attacks
+
+
+# Bubble Sort
+def bubble_sort(offsprings, attacks):
+    swapped = False
+
+    for n in range(len(attacks)-1, 0, -1):
+        for i in range(n):
+            if attacks[i] > attacks[i + 1]:
+                swapped = True
+                attacks[i], attacks[i + 1] = attacks[i + 1], attacks[i]
+                offsprings[i], offsprings[i + 1] = offsprings[i + 1], offsprings[i]             
+        if not swapped:
+            break
+        
     return offsprings
 
 
-def assign_level(children, level):
-    for child in children:
-
-        node = root.get_node(child)
-        node.level = level
-
-
 def attacks(configuration):
-    queens = len(configuration) # n
     attacks = 0
     
-    for i in range(1, queens): # for i = 1; i < n; i++
-        for j in range(i+1, queens+1): # for j = i+1; j <= n; j++
+    for i in range(1, n): # for i = 1; i < n; i++
+        for j in range(i+1, n+1): # for j = i+1; j <= n; j++
             if configuration[i-1] == configuration[j-1]:
                 attacks += 2
             if abs(i - j) == abs(configuration[i-1] - configuration[j-1]):
@@ -104,7 +86,7 @@ def attacks(configuration):
 ###########
 def main():
     print("# Calculemos el tablero para ", n, " reinas #")
-    dls(f, max_tries)
+    greedys(f)
 
 
 if __name__=="__main__": 
